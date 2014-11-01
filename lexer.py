99ce4398e846c94cb9650a8424d84c4fe722f5da
Saran Tunyasuvunakool <saran@saran.in.th>
2014-11-01 17:38:39 +0000

    Replaced the regex lexer with a state machine, now supports [[...]] multidim array expansion construct

--- lexer.py
+++ lexer.py
@@ -1,134 +1,300 @@
 import re
 
-class Tokens(object):
+class TokensSequence(object):
 	def __init__(self, tokens):
 		self.tokens = tokens
 
 	def print_debug(self):
-		for i in self:
-			print i
+		for token in self:
+			token.print_debug()
 
 	def __iter__(self):
 		return iter(self.tokens)
 
-class TextToken(object):
-	def __init__(self, string, line, char):
-		self.text = string
-		self.line = line
-		self.char = char
+	def __getitem__(self, key):
+		return self.tokens[key]
+
+	def __contains__(self, item):
+		return (item in self.tokens)
+
+	def __len__(self):
+		return len(self.tokens)
 
 	def __repr__(self):
-		return repr(self.line) + ':' + repr(self.char) + ':\t' + self.text
+		return 'TokensSequence' + repr(self.tokens)
+
+class TextToken(object):
+	def __init__(self, line, char, text):
+		self.line = line
+		self.char = char
+		self.text = text
+
+	def print_debug(self, prefix = ''):
+		print repr(self.line) + ':' + repr(self.char) + ':\t' + prefix + ' ' + self.text
+
+	def __repr__(self):
+		return '<TextToken>'
+
+
+class ArrayExpandToken(object):
+	def __init__(self, line, char, inner_tokens):
+		self.line = line
+		self.char = char
+		self.inner_tokens = inner_tokens
+
+	def print_debug(self, prefix = ''):
+		print repr(self.line) + ':' + repr(self.char) + ':\t' + prefix + ' [[ ArrayExpand ]]'
+		for token in self.inner_tokens:
+			token.print_debug(prefix + '>>>>')
+
+	def __repr__(self):
+		return '<[[' + repr(self.inner_tokens) + ']]>'
+
 
 class TagToken(object):
-	def __init__(self, string, line, char):
-		self.name = string
+	def __init__(self, line, char, name, args = []):
 		self.line = line
 		self.char = char
+		self.name = name
+		self.args = args
+
+	def print_debug(self, prefix = ''):
+		print repr(self.line) + ':' + repr(self.char) + ':\t' + prefix + '@' + self.name
+		prefix = prefix + '>>>>'
+		for arg in self.args:
+			print repr(arg[0].line) + ':' + repr(arg[0].char) + ':\t' + prefix + ' [ TagArg ]'
+			for token in arg:
+				token.print_debug(prefix + '>>>>')
 
 	def __repr__(self):
-		return repr(self.line) + ':' + repr(self.char) + ':\t' + '@' + self.name
+		return '<@' + self.name + repr(self.args) + '>'
 
-class ParamToken(object):
-	def __init__(self, string, line, char):
-		self.text = string
-		self.line = line
-		self.char = char
 
-	def __repr__(self):
-		return repr(self.line) + ':' + repr(self.char) + ':\t' + '[' + self.text + ']'
+class LexerState(object):
+	TEXT = 0
+	TAG = 1
+	ARRAYEXPAND = 2
 
-def lex(string):
+class Lexer(object):
+	def __init__(self, string, start_line = 1, start_char = 1):
+		self.string = string
+		self.chars_read = 0
 
-	tokens = []
+		self.state = LexerState.TEXT
+		self.tokens = []
 
-	# Matches one line, works with either Windows or UNIX linebreak
-	readline = re.compile(r"([^\r\n]*)(\r\n|\n)?")
+		self.char = ''
+		self.line_number = start_line
+		self.char_pos = start_char
 
-	line_number = 1
+		self.advance()
 
-	while len(string) > 0:
+	def append_token(self, token):
+		self.tokens.append(token)
 
-		# Read a single line from the string and pass it to the line lexer
-		m = readline.match(string)
-		if m is not None:
-			tokens = tokens + lex_line(m.group(1), line_number)
-			string = string[m.end(0):]
-			line_number = line_number + 1
+	def get_tokens(self):
+		if len(self.tokens) == 0:
+			return None
+		elif len(self.tokens) == 1:
+			return self.tokens[0]
 		else:
-			raise Exception("GRX lexer failed unexpectedly")
+			return TokensSequence(self.tokens)
+
+	def advance(self):
+		if self.chars_read < len(self.string):
+			if self.char == '\n':
+				self.line_number = self.line_number + 1
+				self.char_pos = 1
+			elif self.char != '':
+				self.char_pos = self.char_pos + 1
+
+			self.char = self.string[self.chars_read]
+			self.chars_read = self.chars_read + 1
+
+		else:
+			self.char = None
+
+		return self
 
 
-	return Tokens(tokens)
+def lex(string, start_line = 1, start_char = 1):
 
-def lex_line(line, line_number):
+	lexer = Lexer(string, start_line, start_char)
 
-	# Treat empty line as a special case, makes life easier later on
-	if len(line) == 0:
-		return [TextToken('', line_number, 1)]
+	while lexer.char is not None:
+		if lexer.state == LexerState.TEXT:
+			lex_text(lexer)
+		elif lexer.state == LexerState.ARRAYEXPAND:
+			lex_arrayexpand(lexer)
+		elif lexer.state == LexerState.TAG:
+			lex_tag(lexer)
 
-	tokens = []
+	return lexer.get_tokens()
 
-	# Matches longest string up to either an @ character or a [[
-	text_regex = re.compile(r"[^\@|(\[\[)]+")
 
-	# Matches longest string of alphanumeric and underscore following an @ character
-	tag_regex = re.compile(r"\@(\w*)")
+# Lex an inert blob of text
+def lex_text(lexer):
 
-	# Matches the longest string between a pair of [___]
-	params_regex = re.compile(r"\[([^\[\]]*)\]")
+	text = ''
 
-	char_pos = 1
+	start_line = lexer.line_number
+	start_char = lexer.char_pos
 
-	while len(line) > 0:
-		matched = False
+	class InnerState(object):
+		TEXT = 0
+		ARRAYEXPAND_WAIT = 1
 
-		# Tokenize a blob of inert text
-		m = text_regex.match(line)
-		if m is not None:
-			matched = True
-			tokens.append(TextToken(m.group(0), line_number, m.start(0) + char_pos))
-			line = line[m.end(0):]
-			char_pos = char_pos + m.end(0)
+	inner_state = InnerState.TEXT
 
-		# Tokenize an @tag
-		m = tag_regex.match(line)
-		if m is not None:
-			matched = True
-			tokens.append(TagToken(m.group(1), line_number, m.start(0) + char_pos))
-			line = line[m.end(0):]
-			char_pos = char_pos + m.end(0)
+	while (lexer.state == LexerState.TEXT) and (lexer.char is not None):
 
-			# Tokenize a parameter list immediately following an @tag
-			m = params_regex.match(line)
-			if m is not None:
-				tokens = tokens + lex_params(m.group(1), line_number, m.start(1) + char_pos)
-				line = line[m.end(0):]
-				char_pos = char_pos + m.end(0)
+		if lexer.char == '@':
+			lexer.state = LexerState.TAG
 
-		if matched == False:
-			# If this ever happens then go fix our regex -- definitely a bug
-			raise Exception("GRX lexer failed unexpectedly in the middle of a line")
+		elif lexer.char == '[':
+			if inner_state == InnerState.ARRAYEXPAND_WAIT:
+				lexer.state = LexerState.ARRAYEXPAND
+			else:
+				inner_state = InnerState.ARRAYEXPAND_WAIT
+				
+		else:
+			if inner_state == InnerState.ARRAYEXPAND_WAIT:
+				text = text + '[' + lexer.char
+				inner_state = InnerState.TEXT
+			else:
+				text = text + lexer.char
+		
+		lexer.advance()
 
-	return tokens
 
-def lex_params(params, line_number, char_pos):
+	if len(text) > 0:
+		lexer.append_token(TextToken(start_line, start_char, text))
+
+# Lex an array expansion directive
+def lex_arrayexpand(lexer):
+
+	text = ''
+
+	line_number = lexer.line_number
+	char_pos = lexer.char_pos
+
+	class InnerState(object):
+		READ = 0
+		CLOSE_WAIT = 1
+
+	inner_state = InnerState.READ
+	opencount = 0
+
+	while (lexer.state == LexerState.ARRAYEXPAND):
+
+		if lexer.char is None:
+			raise Exception('Error: ' + str(line_number) + ':' + str(char_pos - 2) + ': missing a closing ]]')
+
+		if lexer.char == '[':
+			text = text + lexer.char
+			opencount = opencount + 1
+
+		elif lexer.char == ']':
+			if opencount > 0:
+				text = text + lexer.char
+				opencount = opencount - 1
+			elif inner_state == InnerState.CLOSE_WAIT:
+				lexer.state = LexerState.TEXT
+			else:
+				inner_state = InnerState.CLOSE_WAIT
+
+		else:
+			if inner_state == InnerState.CLOSE_WAIT:
+				text = text + ']' + lexer.char
+				inner_state = InnerState.READ
+			else:
+				text = text + lexer.char
+
+		lexer.advance()
 	
-	tokens = []
+	# Process the expansion string as if it were a separate GRX document
+	inner_tokens = lex(text, line_number, char_pos)
+	lexer.append_token(ArrayExpandToken(line_number, char_pos - 2, inner_tokens))
 
-	separator_regex = re.compile(r"\s*,\s*")
-	orig_len = len(params)
-	params = params.lstrip()
-	char_pos = char_pos + (orig_len - len(params))
 
-	while params is not None:
-		m = separator_regex.search(params)
-		if m is not None:
-			tokens.append(ParamToken(params[0:m.start(0)], line_number, char_pos))
-			params = params[m.end(0):]
-			char_pos = char_pos + m.end(0)
+# Lex an @tag
+def lex_tag(lexer):
+
+	name = ''
+	args = []
+
+	line_number = lexer.line_number
+	char_pos = lexer.char_pos
+
+	# Match only [a-zA-Z0-9_] for a tag name
+	tagname = re.compile(r"\w")
+
+	while (lexer.state == LexerState.TAG) and (lexer.char is not None):
+
+		if tagname.match(lexer.char) is not None:
+			name = name + lexer.char
+			lexer.advance()
+		
 		else:
-			tokens.append(ParamToken(params.rstrip(), line_number, char_pos))
-			params = None
+			if lexer.char == '[':
+				args = lex_tag_args(lexer.advance())
 
-	return tokens
+			lexer.state = LexerState.TEXT
+		
+	lexer.append_token(TagToken(line_number, char_pos - 1, name, args))
+
+
+# Lex arguments list for an @tag
+def lex_tag_args(lexer):
+
+	args = []
+
+	string = ''
+	opencount = 0
+
+	start_line = lexer.line_number
+	start_char = lexer.char_pos
+
+	arg_line = lexer.line_number
+	arg_char = lexer.char_pos
+
+	while opencount >= 0:
+
+		if lexer.char is None:
+			raise Exception('Error: ' + str(start_line) + ':' + str(start_char - 1) + ': @tag argument list is missing a closing ]')
+
+		param_fully_read = False
+
+		# Need this to handle nested [...] inside our arguments list
+		if lexer.char == '[':
+			opencount = opencount + 1
+
+		elif lexer.char == ']':
+			if opencount == 0:
+				# Genuine end of arguments list, treat whatever string we have in the buffer as the last argument
+				param_fully_read = True
+
+			opencount = opencount - 1
+
+		# Commas appearing inside inner brackets are NOT argument delimiters!
+		elif (lexer.char == ',') and (opencount == 0):
+			param_fully_read = True
+
+		if param_fully_read:
+
+			# Process each complete argument string as if it were a separate GRX document
+			inner_tokens = lex(string, arg_line, arg_char)
+			args.append(inner_tokens)
+			
+			# Start a new argument string
+			string = ''
+			lexer.advance()
+			arg_line = lexer.line_number
+			arg_char = lexer.char_pos
+
+		else:
+			# Keep reading into the current argument string
+			string = string + lexer.char
+			lexer.advance()
+
+	return args
